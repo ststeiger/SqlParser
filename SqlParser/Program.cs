0ff9f75b04fd618ef558df1a3439e2fa2c3f0417
root <me@example.com>
2019-09-11 18:34:10 +0200

    Include whitespace

--- SqlParser/Program.cs
+++ SqlParser/Program.cs
@@ -1,38 +1,35 @@
 ﻿
+using Antlr4.Runtime.Misc;
 using SqlParser.grammars;
-
 
 
 namespace SqlParser
 {
-
-
-    class Program
+    
+    
+    internal class Program
     {
-
         // https://www.nuget.org/packages/Microsoft.SqlServer.TransactSql.ScriptDom/15.0.4200.1
-
-
-
+        
+        
         // https://fullboarllc.com/getting-started-with-visual-studio-2015-antlr4-c-and-a-sql-grammar/
         // https://fullboarllc.com/using-antlr-4-with-net-core-2-1-and-c-getting-started/
         // https://fullboarllc.com/antlr4-dotnet-core-visitor/
-
+        
         // https://programming-pages.com/2013/12/14/antlr-4-with-c-and-visual-studio-2012/
         // https://weekly-geekly.github.io/articles/346038/index.html
         // https://tomassetti.me/antlr-mega-tutorial/
         // https://tomassetti.me/getting-started-with-antlr-in-csharp/
-
-
+        
+        
         // https://github.com/elshev/DataGenerator
         // https://github.com/ajaxx/tsql2pgsql
         // https://github.com/azraelrabbit/SqlSchemer
-
         static void Main(string[] args)
         {
             // https://github.com/antlr/grammars-v4/tree/master/tsql
             // https://github.com/antlr/grammars-v4/tree/master/plsql/CSharp
-
+            
             // string text = System.IO.File.ReadAllText(@"D:\username\Desktop\sysflang.sql");
             string text = @"
 SELECT 123 AS /*some crap*/aaa, 'test' as test 
@@ -40,11 +37,9 @@ SELECT 123 AS /*some crap*/aaa, 'test' as test
 --ciao
 -- bye bye
 ";
-
-
+            
             // Dom.Test();
-
-
+            
             LexerTest(text);
             // WalkerTest(fileName);
             // VisitorTest(fileName);
@@ -52,7 +47,6 @@ SELECT 123 AS /*some crap*/aaa, 'test' as test
             System.Console.WriteLine(" --- Press any key to continue --- ");
             System.Console.ReadKey();
         } // End Sub Main 
-
 
 
         // https://github.com/dotjpg3141/Strings
@@ -66,44 +60,61 @@ SELECT 123 AS /*some crap*/aaa, 'test' as test
 
                 Antlr4.Runtime.ICharStream input1 = new Antlr4.Runtime.AntlrInputStream(reader);
                 Antlr4.Runtime.CaseChangingCharStream input = new Antlr4.Runtime.CaseChangingCharStream(input1, true);
-                
+
 
                 TSqlLexer lexer = new TSqlLexer(input);
-                
+
                 Antlr4.Runtime.CommonTokenStream tokenStream = new Antlr4.Runtime.CommonTokenStream(lexer);
-
+                
                 tokenStream.Fill();
-
-                string sql = "";
-
+                
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                int lastIndex = 0;
+                
                 foreach (Antlr4.Runtime.IToken token in tokenStream.GetTokens())
                 {
                     // System.Console.WriteLine(token.Text);
                     string tokenTypeName = lexer.Vocabulary.GetSymbolicName(token.Type);
+                    
+                    if (token.Type == TSqlLexer.LINE_COMMENT || token.Type == TSqlLexer.COMMENT ||
+                        token.Type == TSqlLexer.Eof)
+                    {
+                        Interval ival2 = new Interval(lastIndex, token.StartIndex - 1);
+                        string extracted2 = token.InputStream.GetText(ival2);
+                        sb.Append(extracted2);
 
-                    if (token.Type == TSqlLexer.LINE_COMMENT || token.Type == TSqlLexer.COMMENT || token.Type == TSqlLexer.Eof)
+                        lastIndex = token.StopIndex + 1;
                         continue;
-
-                    sql += token.Text + " ";
+                    } // End if comment 
+                    
+                    // sql += token.Text + " ";
+                    Interval ival = new Interval(lastIndex, token.StopIndex);
+                    string extracted = token.InputStream.GetText(ival);
+                    // System.Console.WriteLine((extracted));
+                    sb.Append(extracted);
+                    
+                    
                     // System.Console.WriteLine(token.Text);
                     // System.Console.WriteLine(token.Type);
                     // System.Console.WriteLine(tokenTypeName);
 
-                }
-
+                    lastIndex = token.StopIndex + 1;
+                } // Next token 
+                
+                string sql = sb.ToString();
+                sb.Clear();
+                sb = null;
                 System.Console.WriteLine(sql);
             }
             catch (System.Exception e)
             {
                 System.Console.WriteLine(e.Message);
             }
-
-        } // End Sub ParserTest 
+        } // End Sub LexerTest 
 
 
         static void WalkerTest(string text)
         {
-
             try
             {
                 System.IO.StringReader reader = new System.IO.StringReader(text);
@@ -111,34 +122,31 @@ SELECT 123 AS /*some crap*/aaa, 'test' as test
                 TSqlLexer lexer = new TSqlLexer(input);
                 Antlr4.Runtime.CommonTokenStream tokens = new Antlr4.Runtime.CommonTokenStream(lexer);
                 TSqlParser parser = new TSqlParser(tokens);
-                //Specify our entry point
+                // Specify our entry point
 
                 // TSqlParser.Query_specificationContext 
-                TSqlParser.Tsql_fileContext Tsql_fileContext1 = parser.tsql_file();
-                /// Antlr4.Runtime.Tree.IParseTree root = (Antlr4.Runtime.Tree.IParseTree)Tsql_fileContext1;
-
-
-
-                // TSqlParser.Query_specificationContext tsqlParser.Tsql_fileContext Tsql_fileContext1 = parser.tsql_file();
-                System.Console.WriteLine("Tsql_fileContext1.ChildCount = " + Tsql_fileContext1.ChildCount.ToString());
-                //Walk it and attach our listener 
+                TSqlParser.Tsql_fileContext fileContext = parser.tsql_file();
+                // Antlr4.Runtime.Tree.IParseTree root = (Antlr4.Runtime.Tree.IParseTree)fileContext;
+                
+                
+                // TSqlParser.Query_specificationContext tsqlParser.Tsql_fileContext fileContext = parser.tsql_file();
+                System.Console.WriteLine("fileContext.ChildCount = " + fileContext.ChildCount.ToString());
+                // Walk it and attach our listener 
                 Antlr4.Runtime.Tree.ParseTreeWalker walker = new Antlr4.Runtime.Tree.ParseTreeWalker();
                 // AntlrTsqListener listener = new AntlrTsqListener();
                 EverythingListener listener = new EverythingListener();
 
-                walker.Walk(listener, Tsql_fileContext1);
+                walker.Walk(listener, fileContext);
             }
             catch (System.Exception e)
             {
                 System.Console.WriteLine(e.Message);
             }
-
-        } // End Sub ParserTest 
+        } // End Sub WalkerTest 
         
-
+        
         static void VisitorTest(string text)
         {
-
             try
             {
                 System.IO.StringReader reader = new System.IO.StringReader(text);
@@ -147,27 +155,25 @@ SELECT 123 AS /*some crap*/aaa, 'test' as test
                 Antlr4.Runtime.CommonTokenStream tokens = new Antlr4.Runtime.CommonTokenStream(lexer);
                 TSqlParser parser = new TSqlParser(tokens);
                 //Specify our entry point
-
+                
                 // TSqlParser.Query_specificationContext 
-                TSqlParser.Tsql_fileContext Tsql_fileContext1 = parser.tsql_file();
-
-                // TSqlParser.Query_specificationContext tsqlParser.Tsql_fileContext Tsql_fileContext1 = parser.tsql_file();
-                System.Console.WriteLine("Tsql_fileContext1.ChildCount = " + Tsql_fileContext1.ChildCount.ToString());
-               
+                TSqlParser.Tsql_fileContext fileContext = parser.tsql_file();
+                
+                System.Console.WriteLine("fileContext.ChildCount = " + fileContext.ChildCount.ToString());
+                
                 SqlVisitor vis = new SqlVisitor();
-                string s = vis.Visit(Tsql_fileContext1);
+                string s = vis.Visit(fileContext);
                 System.Console.WriteLine(s);
-
             }
             catch (System.Exception e)
             {
                 System.Console.WriteLine(e.Message);
             }
-
-        } // End Sub ParserTest 
-
-
+            
+        } // End Sub VisitorTest
+        
+        
     } // End Class Program 
-
-
-} // End Namespace SqlParser
+    
+    
+} // End Namespace SqlParser 
